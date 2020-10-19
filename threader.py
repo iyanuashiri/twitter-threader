dc38ddb8a84afc4028ef50e2a22549af357a09ac
Iyanuoluwa Ajao <ajaoiyanu@gmail.com>
2020-10-19 23:35:25 +0100

    Add thread class

---
+++ threader.py
@@ -0,0 +1,31 @@
+import tweepy
+
+from settings import consumer_key, consumer_secret, access_token_key, access_token_secret
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token_key, access_token_secret)
+
+api = tweepy.API(auth)
+
+
+class Thread:
+    def __init__(self, status_id):
+        self.status_id = status_id
+
+    def get_thread(self, status_id=None, thread=None):
+        status = api.get_status(self.status_id, tweet_mode='extended') if status_id is None else api.get_status(status_id, tweet_mode='extended')
+        thread = [] if thread is None else thread
+        status_id = status.in_reply_to_status_id
+        tweet = str(status.full_text)
+        thread.append(tweet)
+        if status_id is None:
+            return thread
+        else:
+            return self.get_thread(status_id, thread)
+
+    def convert_to_post(self):
+        thread = self.get_thread()
+        thread - reversed(thread)
+        post = " ".join(thread)
+        return post
