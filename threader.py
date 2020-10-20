56f72afab485844880efb67e063bd36f9c9ae899
Iyanuoluwa Ajao <ajaoiyanu@gmail.com>
2020-10-20 12:58:57 +0100

    Refactor

--- threader.py
+++ threader.py
@@ -1,6 +1,14 @@
-import tweepy
+import os
 
-from twitter_threader.settings import consumer_key, consumer_secret, access_token_key, access_token_secret
+import tweepy
+from dotenv import load_dotenv
+load_dotenv()
+
+
+consumer_key = os.getenv("CONSUMER_KEY")
+consumer_secret = os.getenv("CONSUMER_SECRET")
+access_token_key = os.getenv("ACCESS_TOKEN")
+access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
