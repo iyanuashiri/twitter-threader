878c7f7545b68341156fe99fa3ac0229bedc728f
Iyanuoluwa Ajao <ajaoiyanu@gmail.com>
2020-10-27 21:47:49 +0100

    v1.2.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/iyanuashiri/twitter-threader'
 EMAIL = 'ajaoiyanu@gmail.com'
 AUTHOR = 'Iyanuoluwa Ajao'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.2.1'
+VERSION = '1.2.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
